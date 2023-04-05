fa32e9f2d1d43da126323846820fdd59a37dffe2
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-04-05 15:01:51 +0100

    feat: add user serializers

--- src/serializers/users.serializer.ts
+++ src/serializers/users.serializer.ts
@@ -1,9 +1,32 @@
 import { z } from "zod";
 
-export const createUserSerializer = z.object({});
+export const createUserSerializer = z.object({
+  name: z.string().min(2).trim(),
+  email: z.string().trim().email("Invalid email format").min(10),
+  cpf: z.string().trim().min(11).max(11),
+  cellPhone: z.string().trim().min(11),
+  bithdate: z.string().trim().min(8),
+  password: z
+    .string()
+    .trim()
+    .regex(/[A-Z]/, "Must contain a capital letter")
+    .regex(/([a-z])/, "Must contain a lowercase")
+    .regex(/(\d)/, "Must contain a number")
+    .regex(/(\W)|_/, "Must contain a special character")
+    .regex(/.{8,}/, "Must contain at least 8 characters"),
+  description: z.string().trim().nullable(),
+  isAdm: z.boolean(),
+  isBuyer: z.boolean(),
+});
 
 export const userResponseSerializer = createUserSerializer
-  .extend({})
+  .extend({
+    id: z.string().uuid(),
+    isActive: z.boolean(),
+    createdAt: z.date(),
+    updatedAt: z.date(),
+    deletedAt: z.date().nullable(),
+  })
   .omit({ password: true });
 
 export const listUsersWithoutPassword = userResponseSerializer.array();
