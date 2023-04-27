5720c1257352845172b47fd5943f9ae66777c113
Petrus Lobato <admpetrusmlb@gmail.com>
2023-04-27 13:12:22 -0300

    Add verification schema

--- src/middlewares/verification.middleware.ts
+++ src/middlewares/verification.middleware.ts
@@ -1,19 +1,21 @@
 import { Request, Response, NextFunction } from "express"
+import { ZodTypeAny, ZodError } from "zod"
 
-
-const serializerDataIsValidMiddleware = (schema: any) => async(req: Request, res: Response, next: NextFunction) => {
+const serializeDataIsValidMiddleware = (schema: ZodTypeAny) => async(req: Request, res: Response, next: NextFunction) => {
     try {
-        const validatedData = await schema.validate(req.body, {
-            abortEarly: false,
-            stripUnknown: true
-        })
+        const validatedData = await schema.parse(req.body)
+
         req.body = validatedData
         return next()
-    } catch (error: any) {
-        return res.status(400).json({
+
+    } catch (error) {
+        if(error instanceof ZodError){
+            return res.status(400).json({
             error: error.errors
         })
+        }
+        
     }
 }
 
-export default serializerDataIsValidMiddleware
+export default serializeDataIsValidMiddleware
