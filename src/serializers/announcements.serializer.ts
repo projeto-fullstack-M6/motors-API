da36efbabab2ad445c562b7961ba1968ed9e4107
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-04-28 15:04:05 +0100

    feat: change migrations and announcement serializer

--- src/serializers/announcements.serializer.ts
+++ src/serializers/announcements.serializer.ts
@@ -4,11 +4,11 @@ import { userResponseSerializer } from "./users.serializer";
 export const createAnnouncementSerializer = z.object({
   brand: z.string().min(2).trim(),
   model: z.string().min(2).trim(),
-  year: z.string().min(4),
-  fuel: z.string().min(2).trim(),
+  year: z.string().min(4).optional(),
+  fuel: z.string().min(2).trim().optional(),
   km: z.string().min(2),
   color: z.string().min(2).trim(),
-  fipePrice: z.string().min(2).trim(),
+  fipePrice: z.string().min(2).trim().optional(),
   price: z.string().min(2).trim(),
   description: z.string().trim().nullable().optional(),
   images: z.any().optional(),
@@ -20,6 +20,7 @@ export const announcementResponseSerializer =
     isActive: z.boolean(),
     createdAt: z.date(),
     updatedAt: z.date(),
+    deletedAt: z.date().nullable(),
     isGoodToSale: z.boolean(),
     user: userResponseSerializer,
   });
