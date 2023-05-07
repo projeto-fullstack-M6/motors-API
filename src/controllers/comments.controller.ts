eef40d5d3070ba09e6e8dae7551bd0fcba42b570
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-05-07 13:31:03 +0100

    feat: add controller and service to update comments

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -4,6 +4,7 @@ import {
   createCommentsService,
   listCommentsService,
 } from "../services/comments";
+import { updateCommentsService } from "../services/comments/updateComments.service";
 
 export const createCommentsController = async (req: Request, res: Response) => {
   const commentsData: ICommentsRequest = req.body;
@@ -21,4 +22,14 @@ export const listCommentsController = async (req: Request, res: Response) => {
   const announcementsId: string = req.params.id;
   const comment = await listCommentsService(announcementsId);
   return res.json(comment);
+};
+
+export const updateCommentsController = async (req: Request, res: Response) => {
+  const commentsData: ICommentsRequest = req.body;
+  const commentId: string = req.params.id;
+  const comment = await updateCommentsService(
+    commentsData,
+    commentId
+  );
+  return res.status(200).json(comment);
 };
--- src/routes/comments.route.ts
+++ src/routes/comments.route.ts
@@ -2,11 +2,20 @@ import { Router } from "express";
 import {
   createCommentsController,
   listCommentsController,
+  updateCommentsController,
 } from "../controllers/comments.controller";
 import { AuthMiddleware } from "../middlewares";
+import { CommentsOrAdOwnerMiddleware } from "../middlewares/comment/commentsOrAdOwner.middleware";
 
 export const commentsRouter = Router();
 
 commentsRouter.post("/:id", AuthMiddleware, createCommentsController);
 
 commentsRouter.get("/:id", listCommentsController);
+
+commentsRouter.patch(
+  "/:id",
+  AuthMiddleware,
+  CommentsOrAdOwnerMiddleware,
+  updateCommentsController
+);
